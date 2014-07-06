82417626393741bf8c2638ca64c48e1907b66006
Daniel Brooks <daniel_brooks@dabsquared.com>
2014-07-06 12:43:19 -0700

    Making branches not split with \ and instead use replace.

--- src/main/java/com/dabsquared/gitlabjenkins/GitLabPushTrigger.java
+++ src/main/java/com/dabsquared/gitlabjenkins/GitLabPushTrigger.java
@@ -79,21 +79,13 @@ public class GitLabPushTrigger extends Trigger<AbstractProject<?, ?>> {
             private Action[] createActions(GitLabPushRequest req) {
                 ArrayList<Action> actions = new ArrayList<Action>();
 
-                String[] branches = req.getRef().split("/");
-
-                String branch = branches[branches.length-1];
+                String branch = req.getRef().replaceAll("refs/heads/", "");
 
                 LOGGER.log(Level.INFO, "GitLab Push Request from branch {0}.", branch);
 
-
-                GitSCM scm = (GitSCM) job.getScm();
-
-                BranchSpec spec = scm.getBranches().get(0);
-                String randomBranchName = spec.getName();
-
                 Map<String, ParameterValue> values = new HashMap<String, ParameterValue>();
-                values.put("gitlabSourceBranch", new StringParameterValue("gitlabSourceBranch", randomBranchName));
-                values.put("gitlabTargetBranch", new StringParameterValue("gitlabTargetBranch", randomBranchName));
+                values.put("gitlabSourceBranch", new StringParameterValue("gitlabSourceBranch", branch));
+                values.put("gitlabTargetBranch", new StringParameterValue("gitlabTargetBranch", branch));
                 values.put("gitlabBranch", new StringParameterValue("gitlabBranch", branch));
 
                 List<ParameterValue> listValues = new ArrayList<ParameterValue>(values.values());
