6cbec651840752e81d69d8d6e253a75a9283ef59
Markus Ebenhoeh <markus@ebenhoeh.at>
2015-11-22 13:54:23 +1100

    remove unused import

--- src/main/java/com/dabsquared/gitlabjenkins/GitLabProjectBranchesService.java
+++ src/main/java/com/dabsquared/gitlabjenkins/GitLabProjectBranchesService.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.gitlab.api.GitlabAPI;
 import org.gitlab.api.models.GitlabBranch;
 import org.gitlab.api.models.GitlabProject;
 
