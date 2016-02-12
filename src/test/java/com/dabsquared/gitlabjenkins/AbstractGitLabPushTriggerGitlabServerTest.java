d9961be90139a92f8e76cd4d5a7ad77848570cbc
Robin Müller <coder-hugo@users.noreply.github.com>
2016-02-13 00:17:47 +0100

    Add functionality to add the new publisher automatically to old jobs if addCiMessage was activated for the GitLabPushTrigger

--- src/test/java/com/dabsquared/gitlabjenkins/AbstractGitLabPushTriggerGitlabServerTest.java
+++ src/test/java/com/dabsquared/gitlabjenkins/AbstractGitLabPushTriggerGitlabServerTest.java
@@ -108,6 +108,7 @@ public abstract class AbstractGitLabPushTriggerGitlabServerTest {
 		boolean ciSkip = false;
 		boolean setBuildDescription = true;
 		boolean addNoteOnMergeRequest = true;
+		boolean addCiMessage = true;
 		boolean addVoteOnMergeRequest = true;
 		boolean acceptMergeRequestOnSuccess = false;
 		String branchFilter = null;
@@ -115,7 +116,7 @@ public abstract class AbstractGitLabPushTriggerGitlabServerTest {
 		String excludeBranchesSpec = null;
 		String targetBranchRegex = null;
 		GitLabPushTrigger gitLabPushTrigger = new GitLabPushTrigger(triggerOnPush, triggerOnMergeRequest,
-				triggerOpenMergeRequestOnPush, ciSkip, setBuildDescription, addNoteOnMergeRequest,
+				triggerOpenMergeRequestOnPush, ciSkip, setBuildDescription, addNoteOnMergeRequest, addCiMessage,
 				addVoteOnMergeRequest, acceptMergeRequestOnSuccess, branchFilter, includeBranchesSpec,
 				excludeBranchesSpec, targetBranchRegex);
 
