d290997de4ca38b08aed67fd06729ec7b2361535
Robin Müller <coder-hugo@users.noreply.github.com>
2016-02-23 23:09:56 +0100

    Move status images to the resources directory so they are easier to access

--- src/main/java/com/dabsquared/gitlabjenkins/webhook/status/StatusPngAction.java
+++ src/main/java/com/dabsquared/gitlabjenkins/webhook/status/StatusPngAction.java
@@ -7,9 +7,11 @@ import hudson.util.HttpResponses;
 import jenkins.model.Jenkins;
 import org.acegisecurity.Authentication;
 import org.acegisecurity.context.SecurityContextHolder;
+import org.apache.commons.io.IOUtils;
 import org.kohsuke.stapler.StaplerResponse;
 
 import java.io.File;
+import java.io.InputStream;
 import java.net.URL;
 
 /**
@@ -22,33 +24,29 @@ class StatusPngAction extends BuildStatusAction {
 
     @Override
     protected void writeStatusBody(StaplerResponse response, AbstractBuild<?, ?> build, BuildStatus status) {
-        Authentication old = SecurityContextHolder.getContext().getAuthentication();
-        SecurityContextHolder.getContext().setAuthentication(ACL.SYSTEM);
         try {
-            URL resourceUrl = new URL(Jenkins.getInstance().getPlugin("gitlab-plugin").getWrapper().baseResourceURL + getStatusImageUrl(status));
             response.setHeader("Expires","Fri, 01 Jan 1984 00:00:00 GMT");
             response.setHeader("Cache-Control", "no-cache, private");
             response.setHeader("Content-Type", "image/png");
-            hudson.util.IOUtils.copy(new File(resourceUrl.toURI()), response.getOutputStream());
+            IOUtils.copy(getStatusImage(status), response.getOutputStream());
             response.flushBuffer();
         } catch (Exception e) {
             throw HttpResponses.error(500, "Could not generate response.");
-        } finally {
-            SecurityContextHolder.getContext().setAuthentication(old);
         }
     }
 
-    private String getStatusImageUrl(BuildStatus status) {
-        if(status == BuildStatus.RUNNING) {
-            return "images/running.png";
-        } else if (status == BuildStatus.SUCCESS) {
-            return "images/success.png";
-        } else if (status == BuildStatus.FAILED) {
-            return "images/failed.png";
-        } else if (status == BuildStatus.UNSTABLE) {
-            return "images/unstable.png";
-        } else {
-            return "images/unknown.png";
+    private InputStream getStatusImage(BuildStatus status) {
+        switch (status) {
+            case RUNNING:
+                return getClass().getResourceAsStream("running.png");
+            case SUCCESS:
+                return getClass().getResourceAsStream("success.png");
+            case FAILED:
+                return getClass().getResourceAsStream("failed.png");
+            case UNSTABLE:
+                return getClass().getResourceAsStream("unstable.png");
+            default:
+                return getClass().getResourceAsStream("unknown.png");
         }
     }
 }
