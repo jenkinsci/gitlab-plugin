912967e716f0252af09a6d9f43bf93568c755191
Robin Müller <coder-hugo@users.noreply.github.com>
2016-04-28 22:50:15 +0200

    Replace the GitLabEnvironmentRunListener by an EnvironmentContributor

---
+++ src/main/java/com/dabsquared/gitlabjenkins/environment/GitLabEnvironmentContributor.java
@@ -0,0 +1,25 @@
+package com.dabsquared.gitlabjenkins.environment;
+
+import com.dabsquared.gitlabjenkins.cause.GitLabWebHookCause;
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.model.EnvironmentContributor;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+
+import javax.annotation.Nonnull;
+import java.io.IOException;
+
+/**
+ * @author Robin Müller
+ */
+@Extension
+public class GitLabEnvironmentContributor extends EnvironmentContributor {
+    @Override
+    public void buildEnvironmentFor(@Nonnull Run r, @Nonnull EnvVars envs, @Nonnull TaskListener listener) throws IOException, InterruptedException {
+        GitLabWebHookCause cause = (GitLabWebHookCause) r.getCause(GitLabWebHookCause.class);
+        if (cause != null) {
+            envs.overrideAll(cause.getData().getBuildVariables());
+        }
+    }
+}
