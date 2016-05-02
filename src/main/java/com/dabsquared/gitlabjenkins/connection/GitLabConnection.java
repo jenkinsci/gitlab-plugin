2aeb5d9b4b8c155450ea9374b8606e79e61e9f83
Robin Müller <coder-hugo@users.noreply.github.com>
2016-05-02 23:39:20 +0200

    Use jenkins credentials-plugin for storing the GitLab API Token

--- src/main/java/com/dabsquared/gitlabjenkins/connection/GitLabConnection.java
+++ src/main/java/com/dabsquared/gitlabjenkins/connection/GitLabConnection.java
@@ -1,6 +1,20 @@
 package com.dabsquared.gitlabjenkins.connection;
 
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.CredentialsStore;
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
+import com.cloudbees.plugins.credentials.domains.Domain;
+import hudson.init.InitMilestone;
+import hudson.init.Initializer;
+import hudson.util.Secret;
+import jenkins.model.Jenkins;
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
 import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Robin Müller
@@ -9,14 +23,16 @@ public class GitLabConnection {
 
     private final String name;
     private final String url;
-    private final String apiToken;
+    private transient String apiToken;
+    // TODO make final when migration code gets removed
+    private String apiTokenId;
     private final boolean ignoreCertificateErrors;
 
     @DataBoundConstructor
-    public GitLabConnection(String name, String url, String apiToken, boolean ignoreCertificateErrors) {
+    public GitLabConnection(String name, String url, String apiTokenId, boolean ignoreCertificateErrors) {
         this.name = name;
         this.url = url;
-        this.apiToken = apiToken;
+        this.apiTokenId = apiTokenId;
         this.ignoreCertificateErrors = ignoreCertificateErrors;
     }
 
@@ -28,11 +44,29 @@ public class GitLabConnection {
         return url;
     }
 
-    public String getApiToken() {
-        return apiToken;
+    public String getApiTokenId() {
+        return apiTokenId;
     }
 
     public boolean isIgnoreCertificateErrors() {
         return ignoreCertificateErrors;
+    }
+
+    @Initializer(after = InitMilestone.PLUGINS_STARTED)
+    public static void migrate() throws IOException {
+        GitLabConnectionConfig descriptor = (GitLabConnectionConfig) Jenkins.getInstance().getDescriptor(GitLabConnectionConfig.class);
+        for (GitLabConnection connection : descriptor.getConnections()) {
+            if (connection.apiTokenId == null) {
+                for (CredentialsStore credentialsStore : CredentialsProvider.lookupStores(Jenkins.getInstance())) {
+                    if (credentialsStore instanceof SystemCredentialsProvider.StoreImpl) {
+                        List<Domain> domains = credentialsStore.getDomains();
+                        connection.apiTokenId = UUID.randomUUID().toString();
+                        credentialsStore.addCredentials(domains.get(0),
+                            new StringCredentialsImpl(CredentialsScope.SYSTEM, connection.apiTokenId, "GitLab API Token", Secret.fromString(connection.apiToken)));
+                    }
+                }
+            }
+        }
+        descriptor.save();
     }
 }
