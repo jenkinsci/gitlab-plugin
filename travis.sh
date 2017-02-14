55540173b48ced47f67e093360e371daf44ff635
Karsten Kraus <Karsten.Kraus@swr.de>
2017-02-14 09:44:16 +0100

    optimized/fixed travis-build

--- travis.sh
+++ travis.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-./mvnw verify -Dmaven.javadoc.skip=true -B
-
 if [ "$TRAVIS_TAG" != "" ]; then
     ./mvnw versions::set -DnewVersion=${TRAVIS_TAG}
-    ./mvnw install -Dmaven.javadoc.skip=true -B
 fi
+
+./mvnw install -Dmaven.javadoc.skip=true -B
 
