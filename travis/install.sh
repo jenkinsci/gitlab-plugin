d6f4028c05b7bf396353ffe6d671f58c81395845
Karsten Kraus <Karsten.Kraus@swr.de>
2017-08-28 12:18:37 +0200

    try to execute unit- and integration tests in parallel using build-matrix

--- travis/install.sh
+++ travis/install.sh
@@ -4,5 +4,5 @@ if [ "$TRAVIS_TAG" != "" ]; then
     ./mvnw versions::set -DnewVersion=${TRAVIS_TAG}
 fi
 
- ./mvnw verify -Dmaven.javadoc.skip=true -B -Pall-tests,integration-test
+ ./mvnw verify -Dmaven.javadoc.skip=true -B -P$TEST_PROFILE
 
