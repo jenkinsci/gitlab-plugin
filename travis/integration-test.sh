f42259b31a94598a2fbe6505028c89f927bc286b
Karsten Kraus <Karsten.Kraus@web.de>
2017-10-18 16:13:30 +0200

    simplified build

--- travis/integration-test.sh
+++ travis/integration-test.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+if [[ -d $DOCKER_CACHE_DIR ]]; then
+    ls $DOCKER_CACHE_DIR/*.tar.gz | xargs -I {file} sh -c "zcat {file} | docker load";
+fi
 
 ./mvnw integration-test -P integration-test -Dgitlab.version=$GITLAB_VERSION -Dfindbugs.skip=true -Dmaven.javadoc.skip=true
