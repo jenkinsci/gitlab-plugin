29beab9176f46529bf576978852c609f8d292712
Karsten Kraus <Karsten.Kraus@web.de>
2017-10-18 16:23:17 +0200

    cosmetics

--- travis/integration-test.sh
+++ travis/integration-test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 if [[ -d $DOCKER_CACHE_DIR ]]; then
+    echo "Loading cached images into docker..."
     ls $DOCKER_CACHE_DIR/*.tar.gz | xargs -I {file} sh -c "zcat {file} | docker load";
 fi
 
