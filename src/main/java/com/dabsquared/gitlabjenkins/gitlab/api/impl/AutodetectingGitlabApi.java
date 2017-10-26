6d9a34dc1bc5674d067d40879cef37881e1dc79d
Karsten Kraus <Karsten.Kraus@web.de>
2017-10-26 23:56:12 +0200

    cosmetic changes

--- src/main/java/com/dabsquared/gitlabjenkins/gitlab/api/impl/AutodetectingGitlabApi.java
+++ src/main/java/com/dabsquared/gitlabjenkins/gitlab/api/impl/AutodetectingGitlabApi.java
@@ -27,7 +27,7 @@ final class AutodetectingGitlabApi implements GitLabApi {
     private final int readTimeout;
     private GitLabApi delegate;
 
-    
+
     AutodetectingGitlabApi(Iterable<GitLabClientBuilder> builders, String url, String token, boolean ignoreCertificateErrors, int connectionTimeout, int readTimeout) {
         this.builders = builders;
         this.url = url;
@@ -39,210 +39,230 @@ final class AutodetectingGitlabApi implements GitLabApi {
 
     @Override
     public Project createProject(final String projectName) {
-        return new GitLabOperation<Project>() {
-            @Override
-            Project execute(GitLabApi api) {
-                return api.createProject(projectName);
-            }
-        }.execute();
+        return execute(
+            new GitLabOperation<Project>() {
+                @Override
+                Project execute(GitLabApi api) {
+                    return api.createProject(projectName);
+                }
+            });
     }
 
     @Override
     public MergeRequest createMergeRequest(final Integer projectId, final String sourceBranch, final String targetBranch, final String title) {
-        return new GitLabOperation<MergeRequest>() {
-            @Override
-            MergeRequest execute(GitLabApi api) {
-                return api.createMergeRequest(projectId, sourceBranch, targetBranch, title);
-            }
-        }.execute();
+        return execute(
+            new GitLabOperation<MergeRequest>() {
+                @Override
+                MergeRequest execute(GitLabApi api) {
+                    return api.createMergeRequest(projectId, sourceBranch, targetBranch, title);
+                }
+            });
     }
 
     @Override
     public Project getProject(final String projectName) {
-        return new GitLabOperation<Project>() {
-            @Override
-            Project execute(GitLabApi api) {
-                return api.getProject(projectName);
-            }
-        }.execute();
+        return execute(
+            new GitLabOperation<Project>() {
+                @Override
+                Project execute(GitLabApi api) {
+                    return api.getProject(projectName);
+                }
+            });
     }
 
     @Override
     public Project updateProject(final String projectId, final String name, final String path) {
-        return new GitLabOperation<Project>() {
-            @Override
-            Project execute(GitLabApi api) {
-                return api.updateProject(projectId, name, path);
-            }
-        }.execute();
+        return execute(
+            new GitLabOperation<Project>() {
+                @Override
+                Project execute(GitLabApi api) {
+                    return api.updateProject(projectId, name, path);
+                }
+            });
     }
 
     @Override
     public void deleteProject(final String projectId) {
-        new GitLabOperation<Void>() {
-            @Override
-            Void execute(GitLabApi api) {
-                api.deleteProject(projectId);
-                return null;
-            }
-        }.execute();
+        execute(
+            new GitLabOperation<Void>() {
+                @Override
+                Void execute(GitLabApi api) {
+                    api.deleteProject(projectId);
+                    return null;
+                }
+            });
     }
 
     @Override
     public void addProjectHook(final String projectId, final String url, final Boolean pushEvents, final Boolean mergeRequestEvents, final Boolean noteEvents) {
-        new GitLabOperation<Void>() {
-            @Override
-            Void execute(GitLabApi api) {
-                api.addProjectHook(projectId, url, pushEvents, mergeRequestEvents, noteEvents);
-                return null;
-            }
-        }.execute();
+        execute(
+            new GitLabOperation<Void>() {
+                @Override
+                Void execute(GitLabApi api) {
+                    api.addProjectHook(projectId, url, pushEvents, mergeRequestEvents, noteEvents);
+                    return null;
+                }
+            });
     }
 
     @Override
     public void changeBuildStatus(final String projectId, final String sha, final BuildState state, final String ref, final String context, final String targetUrl, final String description) {
-        new GitLabOperation<Void>() {
-            @Override
-            Void execute(GitLabApi api) {
-                api.changeBuildStatus(projectId, sha, state, ref, context, targetUrl, description);
-                return null;
-            }
-        }.execute();
+        execute(
+            new GitLabOperation<Void>() {
+                @Override
+                Void execute(GitLabApi api) {
+                    api.changeBuildStatus(projectId, sha, state, ref, context, targetUrl, description);
+                    return null;
+                }
+            });
     }
 
     @Override
     public void changeBuildStatus(final Integer projectId, final String sha, final BuildState state, final String ref, final String context, final String targetUrl, final String description) {
-        new GitLabOperation<Void>() {
-            @Override
-            Void execute(GitLabApi api) {
-                api.changeBuildStatus(projectId, sha, state, ref, context, targetUrl, description);
-                return null;
-            }
-        }.execute();
+        execute(
+            new GitLabOperation<Void>() {
+                @Override
+                Void execute(GitLabApi api) {
+                    api.changeBuildStatus(projectId, sha, state, ref, context, targetUrl, description);
+                    return null;
+                }
+            });
     }
 
     @Override
     public void getCommit(final String projectId, final String sha) {
-        new GitLabOperation<Void>() {
-            @Override
-            Void execute(GitLabApi api) {
-                api.getCommit(projectId, sha);
-                return null;
-            }
-        }.execute();
+        execute(
+            new GitLabOperation<Void>() {
+                @Override
+                Void execute(GitLabApi api) {
+                    api.getCommit(projectId, sha);
+                    return null;
+                }
+            });
     }
 
     @Override
     public void acceptMergeRequest(final Integer projectId, final Integer mergeRequestId, final String mergeCommitMessage, final boolean shouldRemoveSourceBranch) {
-        new GitLabOperation<Void>() {
-            @Override
-            Void execute(GitLabApi api) {
-                api.acceptMergeRequest(projectId, mergeRequestId, mergeCommitMessage, shouldRemoveSourceBranch);
-                return null;
-            }
-        }.execute();
+        execute(
+            new GitLabOperation<Void>() {
+                @Override
+                Void execute(GitLabApi api) {
+                    api.acceptMergeRequest(projectId, mergeRequestId, mergeCommitMessage, shouldRemoveSourceBranch);
+                    return null;
+                }
+            });
     }
 
     @Override
     public void createMergeRequestNote(final Integer projectId, final Integer mergeRequestId, final String body) {
-        new GitLabOperation<Void>() {
-            @Override
-            Void execute(GitLabApi api) {
-                api.createMergeRequestNote(projectId, mergeRequestId, body);
-                return null;
-            }
-        }.execute();
+        execute(
+            new GitLabOperation<Void>() {
+                @Override
+                Void execute(GitLabApi api) {
+                    api.createMergeRequestNote(projectId, mergeRequestId, body);
+                    return null;
+                }
+            });
     }
 
     @Override
     public List<MergeRequest> getMergeRequests(final String projectId, final State state, final int page, final int perPage) {
-        return new GitLabOperation<List<MergeRequest>>() {
-            @Override
-            List<MergeRequest> execute(GitLabApi api) {
-                return api.getMergeRequests(projectId, state, page, perPage);
-            }
-        }.execute();
+        return execute(
+            new GitLabOperation<List<MergeRequest>>() {
+                @Override
+                List<MergeRequest> execute(GitLabApi api) {
+                    return api.getMergeRequests(projectId, state, page, perPage);
+                }
+            });
     }
 
     @Override
     public List<Branch> getBranches(final String projectId) {
-        return new GitLabOperation<List<Branch>>() {
-            @Override
-            List<Branch> execute(GitLabApi api) {
-                return api.getBranches(projectId);
-            }
-        }.execute();
+        return execute(
+            new GitLabOperation<List<Branch>>() {
+                @Override
+                List<Branch> execute(GitLabApi api) {
+                    return api.getBranches(projectId);
+                }
+            });
     }
 
     @Override
     public Branch getBranch(final String projectId, final String branch) {
-        return new GitLabOperation<Branch>() {
-            @Override
-            Branch execute(GitLabApi api) {
-                return api.getBranch(projectId, branch);
-            }
-        }.execute();
+        return execute(
+            new GitLabOperation<Branch>() {
+                @Override
+                Branch execute(GitLabApi api) {
+                    return api.getBranch(projectId, branch);
+                }
+            });
     }
 
     @Override
     public void headCurrentUser() {
-        new GitLabOperation<Void>() {
-            @Override
-            Void execute(GitLabApi api) {
-                api.headCurrentUser();
-                return null;
-            }
-        }.execute();
+        execute(
+            new GitLabOperation<Void>() {
+                @Override
+                Void execute(GitLabApi api) {
+                    api.headCurrentUser();
+                    return null;
+                }
+            });
     }
 
     @Override
     public User getCurrentUser() {
-        return new GitLabOperation<User>() {
-            @Override
-            User execute(GitLabApi api) {
-                return api.getCurrentUser();
-            }
-        }.execute();
+        return execute(
+            new GitLabOperation<User>() {
+                @Override
+                User execute(GitLabApi api) {
+                    return api.getCurrentUser();
+                }
+            });
     }
 
     @Override
     public User addUser(final String email, final String username, final String name, final String password) {
-        return new GitLabOperation<User>() {
-            @Override
-            User execute(GitLabApi api) {
-                return api.addUser(email, username, name, password);
-            }
-        }.execute();
+        return execute(
+            new GitLabOperation<User>() {
+                @Override
+                User execute(GitLabApi api) {
+                    return api.addUser(email, username, name, password);
+                }
+            });
     }
 
     @Override
     public User updateUser(final String userId, final String email, final String username, final String name, final String password) {
-        return new GitLabOperation<User>() {
-            @Override
-            User execute(GitLabApi api) {
-                return api.updateUser(userId, email, username, name, password);
-            }
-        }.execute();
+        return execute(
+            new GitLabOperation<User>() {
+                @Override
+                User execute(GitLabApi api) {
+                    return api.updateUser(userId, email, username, name, password);
+                }
+            });
     }
 
     @Override
     public List<Label> getLabels(final String projectId) {
-        return new GitLabOperation<List<Label>>() {
-            @Override
-            List<Label> execute(GitLabApi api) {
-                return api.getLabels(projectId);
-            }
-        }.execute();
+        return execute(
+            new GitLabOperation<List<Label>>() {
+                @Override
+                List<Label> execute(GitLabApi api) {
+                    return api.getLabels(projectId);
+                }
+            });
     }
 
     @Override
     public List<Pipeline> getPipelines(final String projectName) {
-        return new GitLabOperation<List<Pipeline>>() {
-            @Override
-            List<Pipeline> execute(GitLabApi api) {
-                return api.getPipelines(projectName);
-            }
-        }.execute();
+        return execute(
+            new GitLabOperation<List<Pipeline>>() {
+                @Override
+                List<Pipeline> execute(GitLabApi api) {
+                    return api.getPipelines(projectName);
+                }
+            });
     }
 
 
@@ -277,12 +297,12 @@ final class AutodetectingGitlabApi implements GitLabApi {
         return null;
     }
 
-    
-    private abstract class GitLabOperation<R> {
-        final R execute() {
-            return execute(false);
-        }
+    private <R> R execute(GitLabOperation<R> operation) {
+        return operation.execute(false);
+    }
 
+
+    private abstract class GitLabOperation<R> {
         private R execute(boolean reset) {
             try {
                 return execute(delegate(reset));
