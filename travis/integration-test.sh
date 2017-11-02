da59b9cba1e41bd9d99cf30b6252c54febedea43
Karsten Kraus <Karsten.Kraus@web.de>
2017-11-02 23:29:56 +0100

    added batch-mode-flag to integration-test-script

--- travis/integration-test.sh
+++ travis/integration-test.sh
@@ -4,4 +4,4 @@ if [[ -d $DOCKER_CACHE_DIR ]]; then
     ls $DOCKER_CACHE_DIR/*.tar.gz | xargs -I {file} sh -c "zcat {file} | docker load";
 fi
 
-./mvnw integration-test -P integration-test -Dgitlab.version=$GITLAB_VERSION -Dfindbugs.skip=true -Dmaven.javadoc.skip=true
+./mvnw integration-test -B -Pintegration-test -Dgitlab.version=$GITLAB_VERSION -Dfindbugs.skip=true -Dmaven.javadoc.skip=true
