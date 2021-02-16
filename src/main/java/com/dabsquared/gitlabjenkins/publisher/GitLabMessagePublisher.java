e71a9202ce4d7355cae76de2123e9f13d455edcb
Owen Mehegan <owen@nerdnetworks.org>
2021-02-16 15:28:09 +1100

    Fix javadoc failures

--- src/main/java/com/dabsquared/gitlabjenkins/publisher/GitLabMessagePublisher.java
+++ src/main/java/com/dabsquared/gitlabjenkins/publisher/GitLabMessagePublisher.java
@@ -40,6 +40,15 @@ public class GitLabMessagePublisher extends MergeRequestNotifier {
 
     /**
      * @deprecated use {@link #GitLabMessagePublisher()} with setters to configure an instance of this class.
+     * @param onlyForFailure Option to only post message on failure
+     * @param replaceSuccessNote Option to replace message on success
+     * @param replaceFailureNote Option to replace message on failure
+     * @param replaceAbortNote Option to replace message on abort
+     * @param replaceUnstableNote Option to replace message on unstable
+     * @param successNoteText Text of message for build success
+     * @param failureNoteText Text of message for build failure
+     * @param abortNoteText Text of message for build abort
+     * @param unstableNoteText Text of message for unstable build
      */
     @Deprecated
     public GitLabMessagePublisher(boolean onlyForFailure, boolean replaceSuccessNote, boolean replaceFailureNote, boolean replaceAbortNote, boolean replaceUnstableNote,
