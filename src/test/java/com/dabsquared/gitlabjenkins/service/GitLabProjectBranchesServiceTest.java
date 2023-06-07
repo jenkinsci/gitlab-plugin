999d9217c1760029c8d9ff76aa1c81c31352c70f
Harsh Pratap Singh <119954739+harsh-ps-2003@users.noreply.github.com>
2023-06-07 18:39:26 +0530

    migrating services tests

--- src/test/java/com/dabsquared/gitlabjenkins/service/GitLabProjectBranchesServiceTest.java
+++ src/test/java/com/dabsquared/gitlabjenkins/service/GitLabProjectBranchesServiceTest.java
@@ -1,59 +1,50 @@
 package com.dabsquared.gitlabjenkins.service;
 
-import static com.dabsquared.gitlabjenkins.gitlab.api.model.builder.generated.BranchBuilder.branch;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
-import com.dabsquared.gitlabjenkins.gitlab.api.model.Branch;
-import java.io.IOException;
-import java.util.ArrayList;
+import com.dabsquared.gitlabjenkins.util.ProjectIdUtil;
 import java.util.Arrays;
 import java.util.List;
+import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.RepositoryApi;
+import org.gitlab4j.api.models.Branch;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class GitLabProjectBranchesServiceTest {
-    private static final List<String> BRANCH_NAMES_PROJECT_B = Arrays.asList("master", "B-branch-1", "B-branch-2");
+
+    @Mock
+    private GitLabApi gitLabApi;
+
+    @Mock
+    private RepositoryApi repositoryApi;
 
     private GitLabProjectBranchesService branchesService;
 
-    private GitLabClientStub clientStub;
-
     @Before
-    public void setUp() throws IOException {
-        clientStub = new GitLabClientStub();
-        clientStub.addBranches("groupOne/A", convert(Arrays.asList("master", "A-branch-1")));
-        clientStub.addBranches("groupOne/B", convert(BRANCH_NAMES_PROJECT_B));
-
-        // never expire cache for tests
-        branchesService = new GitLabProjectBranchesService();
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        branchesService = GitLabProjectBranchesService.instance();
     }
 
     @Test
-    public void shouldReturnBranchNamesFromGitlabApi() {
-        // when
-        List<String> actualBranchNames = branchesService.getBranches(clientStub, "git@git.example.com:groupOne/B.git");
+    public void shouldReturnBranchNamesFromGitlabApi() throws Exception {
 
-        // then
-        assertThat(actualBranchNames, is(BRANCH_NAMES_PROJECT_B));
-    }
+        String sourceRepository = "git@git.example.com:groupOne/B.git";
+        String projectId = "groupOne/B";
+        List<Branch> branches = Arrays.asList(
+                new Branch().withName("master"),
+                new Branch().withName("B-branch-1"),
+                new Branch().withName("B-branch-2"));
 
-    @Test
-    public void shouldNotMakeUnnecessaryCallsToGitlabApiGetBranches() {
-        // when
-        branchesService.getBranches(clientStub, "git@git.example.com:groupOne/A.git");
-
-        // then
-        assertEquals(1, clientStub.calls("groupOne/A", Branch.class));
-        assertEquals(0, clientStub.calls("groupOne/B", Branch.class));
-    }
-
-    private List<Branch> convert(List<String> branchNames) {
-        ArrayList<Branch> result = new ArrayList<>();
-        for (String branchName : branchNames) {
-            result.add(branch().withName(branchName).build());
-        }
-        return result;
+        when(ProjectIdUtil.retrieveProjectId(gitLabApi, sourceRepository)).thenReturn(projectId);
+        when(gitLabApi.getRepositoryApi()).thenReturn(repositoryApi);
+        when(repositoryApi.getBranches(projectId)).thenReturn(branches);
+        List<String> actualBranchNames = branchesService.getBranches(gitLabApi, sourceRepository);
+        assertThat(actualBranchNames, is(Arrays.asList("master", "B-branch-1", "B-branch-2")));
     }
 }
