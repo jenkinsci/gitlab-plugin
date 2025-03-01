136e577d73267081cf72603218deb0af24be2ebc
Mark Waite <mark.earl.waite@gmail.com>
2025-03-01 14:41:12 -0700

    Use Java 17 @Serial annotation
    Annotation that may allow future checks for serialization.  No real
    benefit today as far as I can tell.

--- src/main/java/com/dabsquared/gitlabjenkins/connection/GitLabCredentialMatcher.java
+++ src/main/java/com/dabsquared/gitlabjenkins/connection/GitLabCredentialMatcher.java
@@ -3,10 +3,12 @@ package com.dabsquared.gitlabjenkins.connection;
 import com.cloudbees.plugins.credentials.Credentials;
 import com.cloudbees.plugins.credentials.CredentialsMatcher;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import java.io.Serial;
 import org.jenkinsci.plugins.plaincredentials.StringCredentials;
 
 public class GitLabCredentialMatcher implements CredentialsMatcher {
 
+    @Serial
     private static final long serialVersionUID = -6684402077086938070L;
 
     @Override
