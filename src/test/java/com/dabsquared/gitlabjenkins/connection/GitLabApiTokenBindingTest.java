4f728255485623e24ccea57006bd17dc0cf69a7d
tiago-teixeira5 <tiago.teixeira@ecorobotix.com>
2025-04-07 12:06:21 +0200

    Support withCredentials on GitLabApiToken(Impl) (#1754)
    * support withCredentials -- initial implementation and tests
    
    * support withCredentials -- move tests + final touches
    
    * support withCredentials -- de-hardcore display name string
    
    * support withCredentials -- use interface instead of implementation class
    
    * support withCredentials -- groovy file from tabs to spaces
    
    * revert test port back to 80
    
    Co-authored-by: Kris Stern <krisstern@outlook.com>
    
    ---------
    
    Co-authored-by: Kris Stern <krisstern@outlook.com>

---
+++ src/test/java/com/dabsquared/gitlabjenkins/connection/GitLabApiTokenBindingTest.java
@@ -0,0 +1,55 @@
+package com.dabsquared.gitlabjenkins.connection;
+
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.CredentialsStore;
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
+import com.cloudbees.plugins.credentials.domains.Domain;
+import hudson.model.Run;
+import hudson.util.Secret;
+import java.io.IOException;
+import java.util.List;
+import org.apache.commons.io.IOUtils;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class GitLabApiTokenBindingTest {
+
+    private static final String API_TOKEN = "secret";
+    private static final String API_TOKEN_ID = "apiTokenId";
+
+    @Rule
+    public JenkinsRule jenkins = new JenkinsRule();
+
+    @Before
+    public void setup() throws IOException {
+        for (CredentialsStore credentialsStore : CredentialsProvider.lookupStores(jenkins.jenkins)) {
+            if (credentialsStore instanceof SystemCredentialsProvider.StoreImpl) {
+                List<Domain> domains = credentialsStore.getDomains();
+                credentialsStore.addCredentials(
+                        domains.get(0),
+                        new GitLabApiTokenImpl(
+                                CredentialsScope.GLOBAL,
+                                API_TOKEN_ID,
+                                "GitLab API Token",
+                                Secret.fromString(API_TOKEN)));
+            }
+        }
+    }
+
+    @Test
+    public void withCredentials_success() throws Exception {
+        WorkflowJob project = jenkins.createProject(WorkflowJob.class);
+        String pipelineText =
+                IOUtils.toString(getClass().getResourceAsStream("pipeline/withCredentials-pipeline.groovy"));
+        project.setDefinition(new CpsFlowDefinition(pipelineText, false));
+        Run<?, ?> build = jenkins.buildAndAssertSuccess(project);
+        // assert false to know we run it in tests
+        jenkins.assertLogContains("Token1 is ecret", build);
+        jenkins.assertLogContains("Token2 is ecret", build);
+    }
+}
