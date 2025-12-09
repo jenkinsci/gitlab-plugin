1afb1d6c51a03f118cb3c38f5cd85d3071bfd62f
Daniel Beck <1831569+daniel-beck@users.noreply.github.com>
2025-12-09 01:30:42 +0100

    Switch to using HTMLUnit-compatible JS (#1846)
    Co-authored-by: Daniel Beck <daniel-beck@users.noreply.github.com>

--- src/main/resources/com/dabsquared/gitlabjenkins/GitLabPushTrigger/adjunct.js
+++ src/main/resources/com/dabsquared/gitlabjenkins/GitLabPushTrigger/adjunct.js
@@ -1,6 +1,8 @@
 Behaviour.specify("BUTTON.gitlab_plugin-generate", "gitlab_plugin-generate", 0, function (e) {
     e.onclick = function (evt) {
-        document.getElementById('gitlab_plugin_secretToken').value = [...Array(32)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+        const array = new Uint32Array(4);
+        self.crypto.getRandomValues(array);
+        document.getElementById('gitlab_plugin_secretToken').value = Array.from(array).map(e => e.toString(16)).join("");
         evt.preventDefault();
     };
 });
