e98fc51ace11640367a23d5776e29b41394f4787
Michael Musenbrock <michael.musenbrock@gmail.com>
2026-03-31 17:12:53 +0200

    Convert the ResteasyGitLabClientBuilderGetCurrentUserTest to RealJenkinsExtension to replicate class loading on real instances

--- src/test/java/com/dabsquared/gitlabjenkins/gitlab/api/impl/ResteasyGitLabClientBuilderGetCurrentUserTest.java
+++ src/test/java/com/dabsquared/gitlabjenkins/gitlab/api/impl/ResteasyGitLabClientBuilderGetCurrentUserTest.java
@@ -1,47 +1,51 @@
 package com.dabsquared.gitlabjenkins.gitlab.api.impl;
 
-import static com.dabsquared.gitlabjenkins.gitlab.api.impl.TestUtility.addGitLabApiToken;
-import static com.dabsquared.gitlabjenkins.gitlab.api.impl.TestUtility.buildClientWithDefaults;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockserver.model.HttpRequest.request;
 import static org.mockserver.model.HttpResponse.response;
 
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.CredentialsStore;
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
+import com.cloudbees.plugins.credentials.domains.Domain;
+import com.dabsquared.gitlabjenkins.connection.GitlabCredentialResolver;
 import com.dabsquared.gitlabjenkins.gitlab.api.GitLabClient;
-import com.dabsquared.gitlabjenkins.gitlab.api.GitLabClientBuilder;
 import com.dabsquared.gitlabjenkins.gitlab.api.model.User;
+import hudson.util.Secret;
 import jakarta.ws.rs.HttpMethod;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+import java.util.function.Consumer;
+import jenkins.model.Jenkins;
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
 import org.jvnet.hudson.test.JenkinsRule;
-import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
+import org.jvnet.hudson.test.junit.jupiter.RealJenkinsExtension;
 import org.mockserver.client.MockServerClient;
 import org.mockserver.junit.jupiter.MockServerExtension;
 
-@WithJenkins
 @ExtendWith(MockServerExtension.class)
 class ResteasyGitLabClientBuilderGetCurrentUserTest {
 
+    private static final String API_TOKEN = "secret";
+    private static final String API_TOKEN_ID = "apiTokenId";
+
     private static MockServerClient mockServerClient;
 
-    private GitLabClient client;
+    @RegisterExtension
+    private final RealJenkinsExtension rule = new RealJenkinsExtension();
 
     @BeforeAll
     static void setUpMockServer(MockServerClient mockServer) {
         mockServerClient = mockServer;
-    }
-
-    @BeforeEach
-    void setUp(@SuppressWarnings("unused") JenkinsRule rule) throws Exception {
-        addGitLabApiToken();
-
-        String gitLabUrl = "http://localhost:" + mockServerClient.getPort() + "/gitlab";
-        GitLabClientBuilder clientBuilder = new ResteasyGitLabClientBuilder("test", 0, V4GitLabApiProxy.class, null);
-        client = buildClientWithDefaults(clientBuilder, gitLabUrl);
     }
 
     @AfterEach
@@ -50,7 +54,7 @@ class ResteasyGitLabClientBuilderGetCurrentUserTest {
     }
 
     @Test
-    void getCurrentUser_deserializesJsonIntoUser() {
+    void getCurrentUser_deserializesJsonIntoUser() throws Throwable {
         String userJson = """
             {
               "id": 42,
@@ -59,26 +63,18 @@ class ResteasyGitLabClientBuilderGetCurrentUserTest {
               "email": "jenkins@noreply.gitlab.example.com"
             }""";
 
-        mockServerClient
-                .when(request()
-                        .withMethod(HttpMethod.GET)
-                        .withPath("/gitlab/api/v4/user")
-                        .withHeader("PRIVATE-TOKEN", TestUtility.API_TOKEN))
-                .respond(response()
-                        .withStatusCode(Response.Status.OK.getStatusCode())
-                        .withHeader("Content-Type", MediaType.APPLICATION_JSON)
-                        .withBody(userJson));
+        setupMockServerClient(userJson);
 
-        User user = client.getCurrentUser();
-
-        assertEquals(42, user.getId());
-        assertEquals("Jen Kins", user.getName());
-        assertEquals("jenkins", user.getUsername());
-        assertEquals("jenkins@noreply.gitlab.example.com", user.getEmail());
+        rule.then(new DeserializesJsonIntoUserStep(mockServerClient.getPort(), user -> {
+            assertEquals(42, user.getId());
+            assertEquals("Jen Kins", user.getName());
+            assertEquals("jenkins", user.getUsername());
+            assertEquals("jenkins@noreply.gitlab.example.com", user.getEmail());
+        }));
     }
 
     @Test
-    void getCurrentUser_ignoresUnknownJsonFields() {
+    void getCurrentUser_ignoresUnknownJsonFields() throws Throwable {
         String userJson = """
             {
               "id": 42,
@@ -121,21 +117,86 @@ class ResteasyGitLabClientBuilderGetCurrentUserTest {
               "preferred_language": "en"
             }""";
 
+        setupMockServerClient(userJson);
+
+        rule.then(new DeserializesJsonIntoUserStep(mockServerClient.getPort(), user -> {
+            assertEquals(42, user.getId());
+            assertEquals("Jen Kins", user.getName());
+            assertEquals("jenkins", user.getUsername());
+            assertEquals("jenkins@noreply.gitlab.example.com", user.getEmail());
+        }));
+    }
+
+    private void setupMockServerClient(final String userResponseJson) {
         mockServerClient
                 .when(request()
                         .withMethod(HttpMethod.GET)
                         .withPath("/gitlab/api/v4/user")
-                        .withHeader("PRIVATE-TOKEN", TestUtility.API_TOKEN))
+                        .withHeader("PRIVATE-TOKEN", API_TOKEN))
                 .respond(response()
                         .withStatusCode(Response.Status.OK.getStatusCode())
                         .withHeader("Content-Type", MediaType.APPLICATION_JSON)
-                        .withBody(userJson));
-
-        User user = client.getCurrentUser();
-
-        assertEquals(42, user.getId());
-        assertEquals("Jen Kins", user.getName());
-        assertEquals("jenkins", user.getUsername());
-        assertEquals("jenkins@noreply.gitlab.example.com", user.getEmail());
+                        .withBody(userResponseJson));
     }
+
+    /**
+     * Serializable test run class that carries the MockServer port into the Jenkins JVM.
+     */
+    private static final class DeserializesJsonIntoUserStep implements RealJenkinsExtension.Step {
+        @Serial
+        private static final long serialVersionUID = 1L;
+
+        private final int port;
+
+        private final SerializableConsumer<User> assertUser;
+
+        public DeserializesJsonIntoUserStep(final int port, final SerializableConsumer<User> assertUser) {
+            this.port = port;
+            this.assertUser = assertUser;
+        }
+
+        private int getPort() {
+            return port;
+        }
+
+        @Override
+        public void run(JenkinsRule r) throws Throwable {
+            addGitLabApiToken();
+
+            User user = buildGitLabClient(getPort()).getCurrentUser();
+
+            assertUser.accept(user);
+        }
+
+        /**
+         * Own version of this utility method, because with {@link RealJenkinsExtension} the test class runs in a
+         * different classloader than the plugin code.
+         */
+        static void addGitLabApiToken() throws Exception {
+            for (CredentialsStore credentialsStore : CredentialsProvider.lookupStores(Jenkins.getInstanceOrNull())) {
+                if (credentialsStore instanceof SystemCredentialsProvider.StoreImpl) {
+                    List<Domain> domains = credentialsStore.getDomains();
+                    credentialsStore.addCredentials(
+                            domains.get(0),
+                            new StringCredentialsImpl(
+                                    CredentialsScope.SYSTEM,
+                                    API_TOKEN_ID,
+                                    "GitLab API Token",
+                                    Secret.fromString(API_TOKEN)));
+                }
+            }
+        }
+
+        /**
+         * Own version of this utility method, because with {@link RealJenkinsExtension} the test class runs in a
+         * different classloader than the plugin code.
+         */
+        static GitLabClient buildGitLabClient(final int port) {
+            String gitLabUrl = "http://localhost:" + port + "/gitlab";
+            return new V4GitLabClientBuilder()
+                    .buildClient(gitLabUrl, new GitlabCredentialResolver(null, API_TOKEN_ID), true, 10, 10);
+        }
+    }
+
+    interface SerializableConsumer<T> extends Consumer<T>, Serializable {}
 }
